76aa62c38d229311f3ad93c3b3b1697bcaf07040
Claudia Castro Faccetti <cfaccetti@gmail.com>
2023-05-05 13:16:04 +0100

    Add Banner and Create Folder Structure

---
+++ src/Habits/Habits.js
@@ -0,0 +1,16 @@
+import './Habits.css';
+
+function Habits() {
+  return (
+    <body>
+    <section className="Section1">
+      <header className="Banner">
+      <h1 className="Section2-header">Wellness Hub</h1>
+      </header>
+    </section>
+    
+    </body>
+  );
+}
+
+export default Habits;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Habits from './Habits/Habits';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Habits />
   </React.StrictMode>
 );
 
