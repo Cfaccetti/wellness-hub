a0b0923ddd99957cff93e2999cfdae9b3735fbfc
Claudia Castro Faccetti <cfaccetti@gmail.com>
2023-05-07 18:14:12 +0100

    Create List of Habits with Delete Button

--- src/Pages/Habits/Habitbox/Habitbox.js
+++ src/Pages/Habits/Habitbox/Habitbox.js
@@ -1,36 +1,50 @@
 import './Habitbox.css';
-import Habitarray from './Habitarray';
+import { useState } from 'react'
             
-let habits = []
+// let habits = []
 
 // const [habits, setHabits] = useState[0];
 
-function handleSubmit() {
-    const habitform = document.querySelector('#habit-form')
-    const habitNameInput = document.querySelector('#hname');
-    const habitTypeInput = document.querySelector('#type');
+// function handleSubmit() {
+    
+    // const habitform = document.querySelector('#habit-form')
+    // const habitNameInput = document.querySelector('#hname');
+    // const habitTypeInput = document.querySelector('#type');
 
-    const name = habitNameInput.value
-    const type = habitTypeInput.value
+    // const name = habitNameInput.value
+    // const type = habitTypeInput.value
             
-    let habit = {
-        name: name,
-        type: type
-    }
+    // let habit = {
+    //     name: name,
+    //     type: type
+    // }
 
-    // setHabits(habit);
-
-    habits.push(habit);
-}
+    // habits.push(habit);
+    // console.table(habits)
+    
+// }
 
 export default function Habitbox() {
-    return (
-        <>
-        <form id="habit-form" className="habitCreator" onSubmit={handleSubmit}>
+
+    const [habit, setHabit] = useState('');
+    const [type, setType] = useState('');
+    const [habits, setHabits] = useState([]);
+
+    console.table(habits)
+
+    return(
+    <div className="form-habit-list">
+        <section className="instructionBox">
+                <h3>Instructions</h3>
+                <p>To add habits to the tracker, write the name of a habit to the text box, select the type of habit and click on save. </p>
+                <p>The updated list of habits will appear on the right hand side of the table.</p>
+        </section>
+        <form id="habit-form" className="habitCreator" onSubmit={(e) => {e.preventDefault(); 
+            setHabits([...habits, {id: Date.now(), habit, type}]) }}>
         <label htmlFor="hname">Habit:</label>
-        <input type="text" id="hname" name="hname" placeholder="Insert new habit"></input>
+        <input type="text" id="hname" name="hname" placeholder="Insert new habit" required value={habit} onChange={e => setHabit(e.target.value)}></input>
         <label htmlFor="type">Type of Habit:</label>
-        <select name="type" id="type" defaultValue=''>
+        <select name="type" id="type" defaultValue='' required value={type} onChange={e => setType(e.target.value)}>
             <option value="" disabled>Select a type</option>
             <option value="Self-care">Self-care</option>
             <option value="Self-growth">Self-growth</option>
@@ -38,8 +52,21 @@ export default function Habitbox() {
         </select>
         <input className="save" type="submit" value="Save"></input>
         </form>
-        <Habitarray />
-        </>
+        <div className='habit-list'>
+        <h3>Habit list</h3>
+        <ul>{habits.map(item => (
+            <li>{item.habit}: {item.type}<button onClick={() => {
+                setHabits(
+                  habits.filter(a =>
+                    a.id !== item.id
+                  )
+                );
+              }}>
+                Delete
+              </button></li>))}
+        </ul>
+        </div>
+    </div>
 )     
 }
 
