c35acf4fcde7560aa485e983cc5531ade60f80ec
Claudia Castro Faccetti <cfaccetti@gmail.com>
2023-05-08 23:23:40 +0100

    Add Count for Each day of Week

--- src/Pages/Habits/Habitbox/Habitbox.js
+++ src/Pages/Habits/Habitbox/Habitbox.js
@@ -1,50 +1,98 @@
 import './Habitbox.css';
 import { useState } from 'react'
-            
-// let habits = []
-
-// const [habits, setHabits] = useState[0];
-
-// function handleSubmit() {
-    
-    // const habitform = document.querySelector('#habit-form')
-    // const habitNameInput = document.querySelector('#hname');
-    // const habitTypeInput = document.querySelector('#type');
-
-    // const name = habitNameInput.value
-    // const type = habitTypeInput.value
-            
-    // let habit = {
-    //     name: name,
-    //     type: type
-    // }
-
-    // habits.push(habit);
-    // console.table(habits)
-    
-// }
+          
 
 export default function Habitbox() {
+
+  // Creates states for habit list
 
     const [habit, setHabit] = useState('');
     const [type, setType] = useState('');
     const [habits, setHabits] = useState([]);
 
-    console.table(habits)
+  // Creates dates for habit table
+
+    let today = new Date();
+    let todayString = today.toLocaleDateString('en-GB', {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    let yesterday = new Date(today.setDate(today.getDate()-1));
+    let yesterdayString = yesterday.toLocaleDateString('en-GB', {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    let twodaysago = new Date(yesterday.setDate(yesterday.getDate()-1));
+    let twodaysagoString = twodaysago.toLocaleDateString('en-GB', {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    let threedaysago = new Date(twodaysago.setDate(twodaysago.getDate()-1));
+    let threedaysagoString = threedaysago.toLocaleDateString('en-GB', {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    let fourdaysago = new Date(threedaysago.setDate(threedaysago.getDate()-1));
+    let fourdaysagoString = fourdaysago.toLocaleDateString('en-GB', {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    let fivedaysago = new Date(fourdaysago.setDate(fourdaysago.getDate()-1));
+    let fivedaysagoString = fivedaysago.toLocaleDateString('en-GB', {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    let sixdaysago = new Date(fivedaysago.setDate(fivedaysago.getDate()-1));
+    let sixdaysagoString = sixdaysago.toLocaleDateString('en-GB', {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+
+  // Creates function to update habit count
+
+    function updateHabitCount(passedHabit, numericalDay) {
+      let newHabits = habits.map(currentHabit => {
+        const countDay = 'count'+numericalDay;
+        if (currentHabit.id === passedHabit.id) {
+          return {...currentHabit, [countDay]: !currentHabit[countDay]}
+        }
+
+        return currentHabit;
+      });
+  
+      console.dir(newHabits)
+      setHabits(newHabits);
+    }
 
     return(
+    <>
     <div className="form-habit-list">
-        <section className="instructionBox">
+            <section className="instructionBox">
                 <h3>Instructions</h3>
                 <p>To add habits to the tracker, write the name of a habit to the text box, select the type of habit and click on save. </p>
                 <p>The updated list of habits will appear on the right hand side of the table.</p>
         </section>
         <form id="habit-form" className="habitCreator" onSubmit={(e) => {e.preventDefault(); 
-            setHabits([...habits, {id: Date.now(), habit, type}]) }}>
+            setHabits([...habits, {id: Date.now(), habit, type, count1: false, count2:false, count3:false, count4:false, count5:false, count6:false, count7:false}]) }}>
+        <h2 className='habit-creator-title'>Create habits</h2>
         <label htmlFor="hname">Habit:</label>
         <input type="text" id="hname" name="hname" placeholder="Insert new habit" required value={habit} onChange={e => setHabit(e.target.value)}></input>
         <label htmlFor="type">Type of Habit:</label>
-        <select name="type" id="type" defaultValue='' required value={type} onChange={e => setType(e.target.value)}>
+        <select name="type" id="type" required value={type} onChange={e => setType(e.target.value)}>
             <option value="" disabled>Select a type</option>
             <option value="Self-care">Self-care</option>
             <option value="Self-growth">Self-growth</option>
@@ -55,7 +103,7 @@ export default function Habitbox() {
         <div className='habit-list'>
         <h3>Habit list</h3>
         <ul>{habits.map(item => (
-            <li>{item.habit}: {item.type}<button onClick={() => {
+            <li key={item.id}>{item.habit}: {item.type}<button onClick={() => {
                 setHabits(
                   habits.filter(a =>
                     a.id !== item.id
@@ -67,6 +115,54 @@ export default function Habitbox() {
         </ul>
         </div>
     </div>
+    <div className='habit-table-section'>
+      <h2 className='habit-tracker-title'>Track your habits</h2>
+      <table className='habit-table'>
+        <tbody>
+        <tr>
+          <th>Type</th>
+          <th>Habit</th>
+          <th>{ todayString }</th>
+          <th>{ yesterdayString }</th>
+          <th> { twodaysagoString } </th>
+          <th> { threedaysagoString } </th>
+          <th> { fourdaysagoString } </th>
+          <th> { fivedaysagoString } </th>
+          <th> { sixdaysagoString } </th>
+        </tr>
+        {habits.map(item => (
+          <tr key={item.id+'table'}>
+            <td>{item.type}</td>
+            <td>{item.habit}</td>
+            <td>
+              <input checked={item.count1} type='checkbox' id={item.id + 'today'} onChange={() => updateHabitCount(item, 1)}></input>
+            </td>
+            <td>
+              <input checked={item.count2} type='checkbox' id={item.id+'yesterday'} onChange={() => updateHabitCount(item, 2)}></input>
+            </td>
+            <td>
+              <input checked={item.count3} type='checkbox' id={item.id+'twodaysago'} onChange={() => updateHabitCount(item, 3)}></input>
+            </td>
+            <td>
+              <input checked={item.count4} type='checkbox' id={item.id+'threedaysago'} onChange={() => updateHabitCount(item, 4)}></input>
+            </td>
+            <td>
+              <input checked={item.count5} type='checkbox' id={item.id+'fourdaysago'} onChange={() => updateHabitCount(item, 5)}></input>
+            </td>
+            <td>
+              <input checked={item.count6} type='checkbox' id={item.id+'fivedaysago'} onChange={() => updateHabitCount(item, 6)}></input>
+            </td>
+            <td>
+              <input checked={item.count7} type='checkbox' id={item.id+'weekago'} value='' onChange={() => updateHabitCount(item, 7)}></input>
+            </td>
+          </tr>))}
+          </tbody>
+      </table>
+    </div>
+    <div className='dashboard-section'>
+          <h2 className='dashboard-title'>Dashboard</h2>
+    </div>
+    </>
 )     
 }
 
--- src/Pages/Habits/Habits.js
+++ src/Pages/Habits/Habits.js
@@ -4,9 +4,9 @@ import Habitbox from './Habitbox/Habitbox';
 function Habits() {
   return (
   <>
-   <div>
-      <h1>Habits</h1>
-   </div>
+   {/* <div>
+      {/* <h1>Habits</h1> */}
+   {/*</div> */}
    <div>
       <Habitbox />
    </div>
